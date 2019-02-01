5501f1b1255e1020cd5af7d713af20b3ebaeb83b
kevin <guimaraesprogramador@gmail.com>
2019-02-01 19:04:34 -0200

    so falta marca as posicao e muda o mapa

--- src/servicço/mapa.js
+++ src/servicço/mapa.js
@@ -1,4 +1,4 @@
-var initialCoordinates = [-20.1515, -44.2011]; // brumandinho
+let initialCoordinates = [-20.1515, -44.2011]; // brumandinho
 //                          latitute e longitute
 var initialZoomLevel = 13;
 
@@ -9,8 +9,18 @@ var map = L.map('map').setView(initialCoordinates, initialZoomLevel);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; Contribuidores do <a href="http://osm.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var grupo = [];
 // corrigir mapa de brumandinho
 d3.csv("/home/kevin/Downloads/mapa-de-kernel-brumandinho/programa/3109006.csv").then(function(data){
     //falta corrigir os datos dos pontos 
-//L.marker([-20.1515,data[1].area --44.2011]).addTo(map).bindPopup("Hello world!").openPopup();
+  
+  for(var i = 0;i<data.length;i++){
+grupo.push(data[i].area);
+  }
+//
 })
+console.log(grupo);
+var coordenadas = [-20.1515,grupo[1]];
+L.marker([-20.1515,grupo[1]]).addTo(map);
+
