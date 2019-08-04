9e981bf1c671d5503ab698464e138c573ff8d430
kevin <guimaraesprogramador@gmail.com>
2019-08-04 19:51:22 -0300

    corrigindo o erro de latitude e longitude

--- src/servico/mapa.js
+++ src/servico/mapa.js
@@ -1,6 +1,6 @@
-let initialCoordinates = [-18.512178, -44.555031]; // brumandinho
+let initialCoordinates = [-20.1515, -44.2011]; // brumandinho
 //                       latitude e a longitude
-var initialZoomLevel = 8;
+var initialZoomLevel = 10;
 
 // create a map in the "map" div, set the view to a given place and zoom
 var map = L.map('map').setView(initialCoordinates, initialZoomLevel);
@@ -19,35 +19,3 @@ function marcar(e){
 
 }
 map.on('click', marcar);
-/* teve uma ideia de fazer uma reconhecimento da area
-de brumadinho com os Dados do Sicar - Sistema Nacional de Cadastro Ambiental Rural
-,o codigo precisa ser trabalhado muito para marcar as localização de latitude e 
-a longitude de cada area mas estou com problema em calcular essas posições, se estivem com voltade 
-pode ser um grande ajuda então obrigado pela  oportunidade de ajuda vocês.
- 
-*/
-let grupo2 = [];
-d3.json("../programa/municipios.json").then(function(resp){
-  for(var i = 0;i<resp.length;i++){
-    grupo2.push(resp[i]);
-  }
-
-});
-let grupo = [];
-d3.csv("../programa/Homicdio-Consumado.csv").then(function(data){ 
-  for(var i = 0;i<data.length;i++){
-      grupo.push(data[i]);
-    
-
-  }
-//
-})
-grupo2.forEach(function(value,index,array){
-  console.log(index + value.latitude);
-  L.marker([value.latitude,value.longitude]).addTo(map).bindPopup("ok em teste");
-
-})
-/*for(var i = 0;i<grupo2.length;i++){
-  
-}*/
-
