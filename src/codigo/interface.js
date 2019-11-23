fdf220e97c9b51ccaf4ae232a3a0d09dce45a737
kevin <guimaraesprogramador@gmail.com>
2019-11-23 14:47:53 -0300

    comerça o mapa de kernel do brasil com o censo de 2010

---
+++ src/codigo/interface.js
@@ -0,0 +1,16 @@
+var principal = {
+    Sql:function(){
+        this.criar_banco = function(){
+
+        }
+        this.carregar_banco = function(){
+
+        }
+    },
+    envio_mensagem:function(){
+
+    },
+    receber_mensagem:function(){
+
+    }
+}
---
+++ src/codigo/zonas_do_Brasil_thread.js
@@ -0,0 +1,20 @@
+class cidadão{
+    constructor(){
+        
+    }
+    thread_principal(){
+
+    }
+    thread_homens(){
+
+    }
+    thread_mulheres(){
+
+    }
+    thread_criança(){
+
+    }
+    thread_idosos(){
+
+    }
+}
