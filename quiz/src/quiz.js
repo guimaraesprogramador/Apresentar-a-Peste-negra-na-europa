4e61424c10fc2fe59e4aead908f81cf645215afe
kévin <guimaraesprogramador@gmail.com>
2020-07-12 19:35:07 -0300

    feito o diagrama de class e sequência  e ajustando algumas coisas.

--- quiz/src/quiz.js
+++ quiz/src/quiz.js
@@ -1,11 +1,12 @@
 class quizjs{
 
     constructor(){
-        this.nome = "Teste de Conhecimento \n ";
-        this.questões = document.createElement("h3");
-        this.questões.id ="questão";
-          this.button = [document.createElement("button"),
-      document.createElement("button")];
+        this.nome = "Teste de Conhecimento";
+        this.quiz = document.createElement("h3");
+        this.quiz.id ="questão";
+          this.button = [document.createElement("input"),
+      document.createElement("input"),
+      document.createElement("input")];
         this.radio =[
         document.createElement("input"),
         document.createElement("input"),
@@ -15,14 +16,20 @@ class quizjs{
         this.radio[0].type = "radio";
         this.radio[1].type = "radio";
         this.radio[2].type = "radio";
-        this.radio[0].id = "quiz"+(1);
-        this.radio[1].id = "quiz"+ (2);
-        this.radio[2].id = "quiz" + (3);
-        this.button[0].id = "quiz"+(4);
-        this.button[0].id = "quiz"+(5);
+        this.radio[0].id = this.radio[0].type.toString() +"quiz"+(1);
+        this.radio[1].id = this.radio[1].type.toString() + "quiz"+ (2);
+        this.radio[2].id = this.radio[2].type.toString() +"quiz" + (3);
+        this.button[0].type = "submit";
+        this.button[1].type = "submit";
+        this.button[2].type = "submit";
+        this.button[0].id = this.button[0].type.toString() +"quiz"+(4);
+        this.button[1].id = this.button[1].type.toString() +"quiz"+(5);
+        this.button[2].id = this.button[2].type.toString() +"quiz"+(6);
+         
         this.problema;
         this.index;
-        this.palavra ;
+        this.palavra;
+        
     }
     get numero(){
         return this.index;
@@ -36,83 +43,103 @@ class quizjs{
     set problema (q){
          this.palavra = q;
     }
-
+  }
+class Carregardados{
+    
     layort(){
   
       
         var input = [ "<input type = "," value ="," id = "," </input>"];
-        var quiz = L.control({ position: "bottomright" });
-        quiz.onAdd = function(map) {
+        this.quiz = L.control({ position: "bottomright" });
+        this.quiz.onAdd = function(map) {
             var index_radio = 0;
-            var div = L.DomUtil.create("div", "quiz");
+            this.div = L.DomUtil.create("div", "quiz");
               
-            switch(questões.numero){
+            switch(quiz.numero){
             case 0:
-            div.innerHTML += '<h4> '+questões.nome +' </h4>'; 
+            this.div.innerHTML += '<h4> '+quiz.nome +' </h4>';
+            quiz.button[index_radio].setAttribute("value","Inicio"); 
+            this.div.innerHTML += input[0] + quiz.button[index_radio].type.toString() +
+            input[1] + quiz.button[index_radio].value + input[2] +
+            quiz.button[index_radio].id + input[2] + "> "+ input[3];
+            this.div.innerHTML += "</br>";
+            
             break;
             case 1:
-            div.innerHTML += '<h3> '+ questões.problema[0] +' </h3>';
-            questões.radio[0].setAttribute("value",questões.problema[1]);
-            questões.radio[1].setAttribute("value",questões.problema[2]);
-            questões.radio[2].setAttribute("value",questões.problema[3]);
+            this.div.innerHTML += '<h3> '+ quiz.problema[0] +' </h3>';
+            quiz.radio[0].setAttribute("value",quiz.problema[1]);
+            quiz.radio[1].setAttribute("value",quiz.problema[2]);
+            quiz.radio[2].setAttribute("value",quiz.problema[3]);
             
-            div.innerHTML +=  input[0] + questões.radio[index_radio].type.toString() +
-            input[1] + questões.radio[index_radio].value +input[2] +
-            questões.radio[index_radio].id +"> "+
-            questões.radio[index_radio].value +input[3];
+            this.div.innerHTML +=  input[0] + quiz.radio[index_radio].type.toString() +
+            input[1] + quiz.radio[index_radio].value +input[2] +
+            quiz.radio[index_radio].id +"> "+
+            quiz.radio[index_radio].value +input[3];
 
             index_radio = index_radio +1;
 
-            div.innerHTML +=  input[0] + questões.radio[index_radio].type.toString() +
-            input[1] + questões.radio[index_radio].value +input[2] +
-            questões.radio[index_radio].id +"> "+
-            questões.radio[index_radio].value +input[3];
+            this.div.innerHTML +=  input[0] + quiz.radio[index_radio].type.toString() +
+            input[1] + quiz.radio[index_radio].value +input[2] +
+            quiz.radio[index_radio].id +"> "+
+            quiz.radio[index_radio].value +input[3];
 
             index_radio = index_radio +1;
             
-            div.innerHTML +=  input[0] + questões.radio[index_radio].type.toString() +
-            input[1] + questões.radio[index_radio].value +input[2] +
-            questões.radio[index_radio].id +"> "+
-            questões.radio[index_radio].value +input[3] +"</br>";
+            this.div.innerHTML +=  input[0] + quiz.radio[index_radio].type.toString() +
+            input[1] + quiz.radio[index_radio].value +input[2] +
+            quiz.radio[index_radio].id +"> "+
+            quiz.radio[index_radio].value +input[3] +"</br>";
             
             break;    
-            }
-            
-            console.log(div);
-            return div;
+            };
+            console.log(this.div);
+            return this.div;
         }
-        quiz.addTo(m.map);
-       
-      
+        
+        this.quiz.addTo(m.map);
+        switch(this.quiz.div.children[1].id){
+           case this.button[0].id.toString():
+           //click no botão inicio.
+        this.quiz.div.children[1].onclick = function(ev){
+            quiz.numero = 1;
+            //carregar o novo layort;
+           // quiz.http();
+        }
+           break;
+       }
     }
   http()
     {
 
          if(window.XMLHttpRequest){
-            var xhtml= new XMLHttpRequest();
-            if(this.numero == undefined)this.index = 0;
-            xhtml.open("GET","./quiz/questões/perguntas.json",true);
-            xhtml.onload = function(){
-                var users = JSON.parse(xhtml.responseText);
-                if(xhtml.readyState == 4  && xhtml.status == "200"){
-                    if(questões.index == 1){
-                         questões.problema = [users.perguntas.primeira,
-                            users.questão.multescola.primeira[0],
-                            users.questão.multescola.primeira[1],
-                            users.questão.multescola.primeira[2]];
+            
+            if(quiz.numero == undefined){
+                quiz.index = 0;
+                this.layort();
+            }
+            else{
+                var xhtml= new XMLHttpRequest();
+                xhtml.open("GET","./quiz/quiz/perguntas.json",true);
+                xhtml.onload = function(){
+                    var users = JSON.parse(xhtml.responseText);
+                    if(xhtml.readyState == 4  && xhtml.status == "200"){
+                        if(quiz.index == 1){
+                             quiz.problema = [users.perguntas.primeira,
+                                users.questão.multescola.primeira[0],
+                                users.questão.multescola.primeira[1],
+                                users.questão.multescola.primeira[2]];
+                        }
+                    dados.layort();
                     }
-                    questões.layort();
                 }
+                
+                xhtml.send(null);
             }
             
-            xhtml.send(null);
          }
     }
-  }
-const questões = new quizjs();
-questões.http();
+}
 
-//   questões.pergunta().then(ordem=>{
-//     questões.layort();
-//   });
-  
+const quiz = new quizjs();
+const dados = new Carregardados();
+dados.http();
